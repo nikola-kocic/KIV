1873c221d833df20dec7f6034504fa87fea893cf
Nikola Kocić <nikolakocic@gmail.com>
2013-05-28 21:58:03 +0200

    Simplify PictureItemData::avoidOutOfScreen and PictureItemData::pointToOrigin methods.

--- src/kiv/widgets/picture_item/pictureitem_data.cpp
+++ src/kiv/widgets/picture_item/pictureitem_data.cpp
@@ -60,17 +60,21 @@ void PictureItemData::avoidOutOfScreen(const QSize &widgetSize)
     {
         m_boundingRect.moveLeft(0);
     }
-    else if ((m_boundingRect.x() <= (m_boundingRect.width() - widgetSize.width()) - ((m_boundingRect.width() - widgetSize.width()) * 2)))
+    else
     {
-        if ((m_boundingRect.width() - widgetSize.width()) - ((m_boundingRect.width() - widgetSize.width()) * 2) <= 0)
+        const qreal widthDiff = widgetSize.width() - m_boundingRect.width();
+        if ((m_boundingRect.x() <= widthDiff))
         {
-            /* I am too far to the left! */
-            m_boundingRect.moveLeft((m_boundingRect.width() - widgetSize.width()) - ((m_boundingRect.width() - widgetSize.width()) * 2));
-        }
-        else
-        {
-            /* I am too far to the right! */
-            m_boundingRect.moveLeft(0);
+            if (widthDiff <= 0)
+            {
+                /* I am too far to the left! */
+                m_boundingRect.moveLeft(widthDiff);
+            }
+            else
+            {
+                /* I am too far to the right! */
+                m_boundingRect.moveLeft(0);
+            }
         }
     }
 
@@ -79,54 +83,46 @@ void PictureItemData::avoidOutOfScreen(const QSize &widgetSize)
     {
         m_boundingRect.moveTop(0);
     }
-    else if ((m_boundingRect.y() <= (m_boundingRect.height() - widgetSize.height()) - ((m_boundingRect.height() - widgetSize.height()) * 2)))
+    else
     {
-        if ((m_boundingRect.height() - widgetSize.height()) - ((m_boundingRect.height() - widgetSize.height()) * 2) <= 0)
+        const qreal heightDiff = widgetSize.height() - m_boundingRect.height();
+        if ((m_boundingRect.y() <= heightDiff))
         {
-            /* I am too far to the top! */
-            m_boundingRect.moveTop((m_boundingRect.height() - widgetSize.height()) - ((m_boundingRect.height() - widgetSize.height()) * 2));
-        }
-        else
-        {
-            /* I am too far to the bottom! */
-            m_boundingRect.moveTop(0);
+            if (heightDiff <= 0)
+            {
+                /* I am too far to the top! */
+                m_boundingRect.moveTop(heightDiff);
+            }
+            else
+            {
+                /* I am too far to the bottom! */
+                m_boundingRect.moveTop(0);
+            }
         }
     }
 }
 
-QPointF PictureItemData::pointToOrigin(const qreal width, const qreal height, const QSize &widgetSize)
+QPointF PictureItemData::pointToOrigin(const qreal width, const qreal height,
+                                       const QSize &widgetSize)
 {
-    const qreal zoomX = (qreal)width / (qreal)m_boundingRect.width();
-    const qreal zoomY = (qreal)height / (qreal)m_boundingRect.height();
+    qreal originX = 0;
+    qreal originY = 0;
 
     if (width > widgetSize.width())
     {
-        const qreal oldX = (m_boundingRect.x() - (m_boundingRect.x() * 2)) + (widgetSize.width() / 2);
-        const qreal oldY = (m_boundingRect.y() - (m_boundingRect.y() * 2)) + (widgetSize.height() / 2);
-
+        const qreal zoomX = (qreal)width / (qreal)m_boundingRect.width();
+        const qreal oldX = (widgetSize.width() / 2) - m_boundingRect.x();
         const qreal newX = oldX * zoomX;
-        const qreal newY = oldY * zoomY;
-
-        const qreal originX = newX - (widgetSize.width() / 2) - ((newX - (widgetSize.width() / 2)) * 2);
-        const qreal originY = newY - (widgetSize.height() / 2) - ((newY - (widgetSize.height() / 2)) * 2);
-
-        return QPointF(originX, originY);
+        originX = (widgetSize.width() / 2) - newX;
     }
-    else
+
+    if (width > widgetSize.width() || height > widgetSize.height())
     {
-        if (height > widgetSize.height())
-        {
-            const qreal oldY = (m_boundingRect.y() - (m_boundingRect.y() * 2)) + (widgetSize.height() / 2);
-
-            const qreal newY = oldY * zoomY;
-
-            const qreal originY = newY - (widgetSize.height() / 2) - ((newY - (widgetSize.height() / 2)) * 2);
-
-            return QPointF(0, originY);
-        }
-        else
-        {
-            return QPointF(0, 0);
-        }
+        const qreal zoomY = (qreal)height / (qreal)m_boundingRect.height();
+        const qreal oldY = (widgetSize.height() / 2) - m_boundingRect.y();
+        const qreal newY = oldY * zoomY;
+        originY = (widgetSize.height() / 2) - newY;
     }
+
+    return QPointF(originX, originY);
 }
